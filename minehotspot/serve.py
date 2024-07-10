096caad2776884b352a2954418ee398a0b1934d6
HairlessVillager <hairlessvillager@qq.com>
2024-07-10 08:31:43 +0800

    fix: serve.py not works on Windows

--- minehotspot/serve.py
+++ minehotspot/serve.py
@@ -6,7 +6,7 @@ import requests
 from src.prefect.flows.collect_tieba import collect_tieba
 
 
-if __name__ == "__main__":
+def bdist_egg():
     # python src/scrapy/setup.py bdist_egg
     result = subprocess.run("bash bdist_egg.sh".split(), capture_output=True, text=True)
     print("result.stdout:")
@@ -18,21 +18,23 @@ if __name__ == "__main__":
     if result.returncode != 0:
         exit(result.returncode)
 
+
+def deploy_spiders():
     # curl http://scrapyd:6800/addversion.json -F project=minehotspot -F version=1.0 -F egg=@dist/minehotspot-1.0-py3.11.egg
     url = f'{os.getenv("SCRAPYD_URL", "http://localhost:6800")}/addversion.json'
-    project_name = 'minehotspot'
-    version = '1.0'
-    egg_file_path = 'src/scrapy/dist/minehotspot-1.0-py3.11.egg'
+    project_name = "minehotspot"
+    version = "1.0"
+    egg_file_path = "src/scrapy/dist/minehotspot-1.0-py3.11.egg"
 
-    with open(egg_file_path, 'rb') as f:
+    with open(egg_file_path, "rb") as f:
         egg_data = f.read()
 
     files = {
-        'egg': ('minehotspot-1.0-py3.11.egg', egg_data, 'application/octet-stream'),
+        "egg": ("minehotspot-1.0-py3.11.egg", egg_data, "application/octet-stream"),
     }
     data = {
-        'project': project_name,
-        'version': version,
+        "project": project_name,
+        "version": version,
     }
 
     response = requests.post(url, files=files, data=data)
@@ -43,5 +45,13 @@ if __name__ == "__main__":
         print(f"{response.status_code=}")
         exit(-1)
 
-    # prefect
+
+if __name__ == "__main__":
+    # FIXME: remove comment in production environment
+    # NOTE: The bash is not work in Windows.
+    # - in Windows, comment this and run `python setup.py bdist_egg` manually.
+    # - in Linux, no comment
+    # -----------
+    # bdist_egg()
+    deploy_spiders()
     collect_tieba("galgame")
